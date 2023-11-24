46ae1401a1cded1756a55930a9cbedb2830238f8
Giorgi Beriashvili <Giorgi_Beriashvili@epam.com>
2023-11-24 17:43:23 +0400

    add search functionality of songs and add debounce hook for prevent fetching songs on every time we type in input

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+export function useDebounce<T>(value: T, delay?: number): T {
+  // This state holds the debounced value
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  // useEffect is used to perform side effects in function components.
+  useEffect(() => {
+    console.log('useEffect runs');
+    // Inside the effect, a timer is set using setTimeout.
+    const timer = setTimeout(() => {
+      console.log('useEffect timeout runs');
+      // When the timer expires, the debounced value is updated with the current value.
+      setDebouncedValue(value);
+    }, delay || 500); // The delay is either the provided delay or a default of 500 milliseconds.
+
+    // The return statement in useEffect is a cleanup function.
+    // It will be called when the component unmounts or when the dependencies (value or delay) change.
+    return () => {
+      console.log('debounce happens');
+      // This cleanup function clears the timer to prevent the execution of the debounced update
+      // if the value changes before the timer expires.
+      clearTimeout(timer);
+    };
+  }, [value, delay]); // The effect depends on the value and delay.
+
+  // The debounced value is returned from the hook.
+  return debouncedValue;
+}
